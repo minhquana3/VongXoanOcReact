08825ccb4112d9011a1cf88ed43ceccba5f2426a
Daniel <t.dung.lhp@gmail.com>
2022-11-04 20:34:12 -0700

    update

--- src/todolist/Task.jsx
+++ src/todolist/Task.jsx
@@ -1,34 +1,21 @@
 import { useState } from "react";
 import Btn from "../Btn";
 
-export default function Task({ children }) {
+export default function Task({ item }) {
   const [completeStatus, setCompleteStatus] = useState(false);
-
+  const changeStatus = () => {
+    setCompleteStatus(!completeStatus);
+  };
   return (
-    <li className="flex items-center gap-3 justify-between">
+    <>
       <p
         className={
           completeStatus ? "text-gray-500 line-through" : "text-gray-500"
         }
       >
-        {children}
+        {item}
       </p>
-      <Btn btnType="outline" fn={() => setCompleteStatus(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4.5 12.75l6 6 9-13.5"
-          />
-        </svg>
-      </Btn>
-    </li>
+      <Btn btnType="outline" fn={changeStatus}></Btn>
+    </>
   );
 }
--- src/todolist/ToDoAddTask.jsx
+++ src/todolist/ToDoAddTask.jsx
@@ -1,22 +1,25 @@
-export default function ToDoAddTask({
-  onTaskTextChange,
-  taskText,
-  onHandleAddTask,
-}) {
+import React, { useState } from "react";
+
+export default function ToDoAddTask({ onHandleAddTask }) {
+  const [taskText, setTaskText] = useState("");
+
+  const handleTaskList = (taskText) => {
+    onHandleAddTask((prev) => [...prev, taskText]);
+  };
   return (
     <div className="flex items-center gap-3 mb-4">
       <input
         type="text"
         className="form-input px-4 py-3 rounded-lg border-gray-400"
         value={taskText}
-        onChange={(e) => onTaskTextChange(e.target.value)}
+        onChange={(e) => setTaskText(e.target.value)}
       />
       <button
         type="button"
         className="px-6 py-4 bg-blue-600 text-blue-50 rounded-md hover:bg-blue-800"
         onClick={() => {
-          onHandleAddTask((taskList) => [...taskList, taskText]);
-          onTaskTextChange("");
+          handleTaskList(taskText);
+          setTaskText("");
         }}
       >
         Add
